1f74b79eec9a8f85a1002282280a5d07c5a3409e
Jim Lightfoot <jim.lightfoot@outlook.com>
2025-05-26 12:03:10 -0700

    Initial commit

---
+++ MondoCore.Azure.ServiceBus.FunctionalTests/ServiceBusQueue.cs
@@ -0,0 +1,20 @@
+﻿using Azure.Identity;
+
+using MondoCore.Azure.TestHelpers;
+
+namespace MondoCore.Azure.ServiceBus.FunctionalTests
+{
+    [TestClass]
+    public sealed class ServiceBusQueueTests
+    {
+        [TestMethod]
+        public async Task ServiceBusQueue_Send()
+        {
+            var config = TestConfiguration.Load();
+
+            var sb = new ServiceBusQueue(config.ConnectionString, "test");
+
+            await sb.Send("Bob's your uncle");
+        }
+    }
+}
