d81dc79cf40d58d4196da0d7d769f9250a6b7018
Jim Lightfoot <jim.lightfoot@outlook.com>
2026-02-17 11:45:16 -0800

    Add generic version

---
+++ MondoCore.Azure.ServiceBus.FunctionalTests/ServiceBusQueue_T.cs
@@ -0,0 +1,28 @@
+﻿
+using Azure.Identity;
+
+using MondoCore.Azure.TestHelpers;
+
+namespace MondoCore.Azure.ServiceBus.FunctionalTests
+{
+    [TestClass]
+    public sealed class ServiceBusQueue_TTests
+    {
+        [TestMethod]
+        public async Task ServiceBusQueue_Send()
+        {
+            var config = TestConfiguration.Load();
+
+            var sb = new ServiceBusQueue<Car>(config.ConnectionString, "test");
+
+            await sb.Send(new Car { Make = "Chevy", Model = "Corvette", Year = 1956 });
+        }
+    }
+
+    file class Car     
+    {
+        public string Make  { get; set; } = "";
+        public string Model { get; set; } = "";
+        public int    Year  { get; set; }
+    }
+}
